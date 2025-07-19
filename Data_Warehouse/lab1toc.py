b5427248c6ca85c6bb017f00c67cfca42b6aa766
Janak Budhathoki <104624878+JanakB@users.noreply.github.com>
2025-07-19 20:02:43 +0545

    Add files via upload

---
+++ Data_Warehouse/lab1toc.py
@@ -0,0 +1,13 @@
+string = 'Janak'
+#getting first character of the string 
+print(string[0])
+#getting last character of the string 
+print(string[-1])
+#getting first 3 characters of a string 
+print(string[:3])
+#getting last 3 characters of a string 
+print(string[-3:])
+#getting a substring of length 3 from 4th character 
+print(string[4:-1])
+#Get a substring which contains all characters except the last 2 and the 1st character 
+print(string[1:-2])
