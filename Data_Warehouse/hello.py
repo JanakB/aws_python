3aa0930b5e85603021140e699900253fca4cdb5f
Janak Budhathoki <104624878+JanakB@users.noreply.github.com>
2025-07-24 10:25:22 +0545

    Add files via upload

---
+++ Data_Warehouse/hello.py
@@ -0,0 +1,13 @@
+
+string="toclab"
+print(string[0])
+print(string[-1])
+print(string[1])
+print(string[2:])
+print(string[0:3])
+print(string[-2:])
+print(string[-5])
+print(string[1:3])
+print(string[2:4])
+print(string[2:5])
+print(string[2:-1])
